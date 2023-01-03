e82eedcacd6a94f6b07abce3266a888573a2f3f7
Lemonade12 <plus2829@gmail.com>
2023-01-03 14:52:30 +0900

    feat: 채팅부분 클라이언트 부분 코드 추가

--- server.ts
+++ server.ts
@@ -6,6 +6,7 @@ import socket from "./socket";
 const routes = require("./src/routes/index");
 
 const app = express();
+const fs = require("fs");
 const db = require("./database/index");
 const http = require("http");
 
@@ -31,6 +32,20 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(routes);
+app.use("/css", express.static("./static/css"));
+app.use("/js", express.static("./static/js"));
+
+app.get("/chat", function (request, response) {
+  fs.readFile("./static/index.html", function (err: any, data: any) {
+    if (err) {
+      response.send("에러");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    }
+  });
+});
 
 const server = http.createServer(app);
 
--- socket.ts
+++ socket.ts
@@ -11,14 +11,15 @@ function Socket(server: http.Server) {
 
   io.on("connection", (socket) => {
     //* ip 정보 얻기
-    const ip = socket.handshake.address;
+    const ip = socket.conn.remoteAddress;
+    console.log(ip);
     console.log("새로운 클라이언트 접속!", ip, socket.id);
     // socket.id 는 소켓 연결된 고유한 클라이언트 식별자라고 보면된다. 채팅방의 입장한 고유한 사람
 
     //* 연결 종료 시
     socket.on("disconnect", () => {
       console.log("클라이언트 접속 해제", ip, socket.id);
-      clearInterval(interval);
+      //clearInterval(interval);
     });
 
     //* 에러 시
@@ -29,12 +30,16 @@ function Socket(server: http.Server) {
     //* 클라이언트로부터 메시지
     socket.on("reply", (data) => {
       console.log(data);
+      socket.emit("news", "server -> client");
+      console.log("server -> client");
     });
     //* 클라이언트로 메세지 보내기
-    const interval = setInterval(() => {
+    /*const interval = setInterval(() => {
       // 3초마다 클라이언트로 메시지 전송
+      console.log("3초마다 전송");
       socket.emit("news", "server -> client");
     }, 3000);
+    */
   });
 }
 
